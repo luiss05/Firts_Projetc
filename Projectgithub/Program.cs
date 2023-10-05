6d9690b85f295ee5a4b7b36142d13d70066723a7
luiss05 <blkns05@gmail.com>
2023-10-05 15:08:01 -0400

    primeiro commit

---
+++ Projectgithub/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Você prefere beber pinga ou morar na bct? ");
+        string resposta = Convert.ToString(Console.ReadLine());
+        if (resposta == "prefiro morar na bct e sair pra beber pinga"  )
+        {
+            Console.WriteLine("A MLK DOIDO");
+        }
+        else
+        {
+            Console.WriteLine("paia");
+        }
+        Console.ReadKey();
+    }
+}
